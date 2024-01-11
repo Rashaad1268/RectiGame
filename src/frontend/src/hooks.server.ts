ee75ed988ce53198aa2e2d2138286b41dabc9542
Rashaad <rashaadzma@gmail.com>
2024-01-11 17:41:52 +0530

    Clean up code

--- src/frontend/src/hooks.server.ts
+++ src/frontend/src/hooks.server.ts
@@ -1,20 +1,20 @@
 import type { Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
-    const url = event.url.pathname.slice();
+    const urlEndpoint = event.url.pathname.slice();
     const authEndpoints = ["auth/", "/auth/login", "/auth/signup", "/welcome"];
 
-    const isLoggedIn = !!event.cookies.get("sessionid"); // Check if it is truthy
+    const isLoggedIn = !!event.cookies.get("sessionid"); // Check if the sessionid cookie exists
 
     if (isLoggedIn) {
         // If the user is logged in and he visits the authentication pages, redirect him to the home page
-        if (authEndpoints.includes(url)) {
+        if (authEndpoints.includes(urlEndpoint)) {
             event.url.pathname = "/";
             return Response.redirect(event.url);
         }
     } else {
         // If the user is not logged in and visits any othe
-        if (!authEndpoints.includes(url)) {
+        if (!authEndpoints.includes(urlEndpoint)) {
             event.url = new URL(`${event.url.origin}/welcome?from=${event.url.pathname}`);
 
             return Response.redirect(event.url);
