6ce8ad8cb5ca631d44633e4d46941a282d93d928
Rashaad <rashaadzma@gmail.com>
2024-02-03 22:36:36 +0530

    Use defaultrouter instead of simple router

--- src/backend/authentication/urls.py
+++ src/backend/authentication/urls.py
@@ -1,12 +1,10 @@
-
-
 from django.urls import path
 from rest_framework import routers
 
 from . import views
 
 
-router = routers.SimpleRouter()
+router = routers.DefaultRouter()
 router.register('users', views.UserViewSet, basename='users')
 
 urlpatterns = [
