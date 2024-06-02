c53f67dbe6aed1c9f80af44e04c3b46100dbb16d
Rashaad <rashaadzma@gmail.com>
2024-06-02 17:14:05 +0530

    smoll improvements

--- src/frontend/src/lib/utils/emojis.ts
+++ src/frontend/src/lib/utils/emojis.ts
@@ -2,7 +2,7 @@ import { fetchApi } from "$lib/api";
 import { customEmojiStore } from "$lib/stores";
 import { get } from "svelte/store";
 
-const emojiRegex = /(<:(\d+):>)|(&lt;:(\d+):&gt;)/g;
+const emojiRegex = /&lt;:(\d+):&gt;/g;
 
 // export function parseMessageContentCustomEmojis(content: string): Array<string> {
 // return content.matchAll(emojiRegex);
@@ -13,11 +13,7 @@ export function parseMessage(content: string) {
     let newContent = String(content);
 
     // eslint-disable-next-line no-constant-condition
-    while (true) {
-        const emoji = emojis.next().value;
-
-        if (!emoji) break;
-
+    for (const emoji of emojis) {
         const emojiHtml = `<span class="size-5 block bg-red-600 object-contain" />`;
 
         newContent = newContent.replace(emoji[0], emojiHtml);
@@ -34,36 +30,31 @@ export async function convertMessageEmojis({ content, topic }: { content: string
     const emojis = content.matchAll(emojiRegex);
     let newContent = String(content);
 
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-        const emoji = emojis.next().value;
-
-        if (!emoji) break;
-
-        const emojiId = parseInt(emoji[4]);
+    for (const emoji of emojis) {
+        const emojiId = parseInt(emoji[1]);
 
         if (!emojiId) {
             continue;
         }
 
-        const emojiFromCache = get(customEmojiStore)[emojiId];
+        let emojiData = get(customEmojiStore)[emojiId];
         let emojiHtml = emoji[0];
 
-        if (emojiFromCache) {
-            emojiHtml = `<img class="size-5 block object-contain" src="${emojiFromCache.image}" alt="${emojiFromCache.name} emoji" />`;
-        } else {
+        if (!emojiData) {
             const response = await fetchApi(`topics/${topic}/emojis/${emojiId}/`);
             
             if (response.ok) {
-                const emojiData = await response.json();
+                emojiData = await response.json();
                 customEmojiStore.update((customEmojis) => {
                     customEmojis[emojiId] = emojiData;
                     return customEmojis;
                 });
-                emojiHtml = `<img class="size-5 object-contain m-0 inline-block" src="${emojiData.image}" alt="${emojiData.name} emoji" />`;
+            } else {
+                // Emoji does not exist...continue
+                continue;
             }
-            
         }
+        emojiHtml = `<img class="size-5 object-contain m-0 inline-block" src="${emojiData.image}" alt="${emojiData.name} emoji" />`;
         newContent = newContent.replace(emoji[0], emojiHtml);
     }
 
